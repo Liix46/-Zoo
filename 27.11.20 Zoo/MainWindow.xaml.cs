41681aabcd50991ee63cdea759626b43abb2028f
dYndioS <kanyesupreme@ukr.net>
2020-11-30 14:57:47 +0200

    1. Добавил таймер; 2. Реализовал окно загрузки, с кнопкой LOADING. После того как загрузится игра, можно нажать кнопку START, и начнется игра; 3. Реализовал шаблон для кнопки LOADING-START; 4. Сделал фиксированный размер окна, так будет лучше; 5. Загрузил иконки для интерфейса; 6. Создан словарь для стилей и шаблонов;

--- 27.11.20 Zoo/MainWindow.xaml.cs
+++ 27.11.20 Zoo/MainWindow.xaml.cs
@@ -13,21 +13,30 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
 
 namespace _27._11._20_Zoo
 {
-    
+   
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer;
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void MainWindow_Initialized(object sender, EventArgs e)
+        {
+        }
+
         private void MainWindow_Loaded(object sender, RoutedEventArgs e)
         {
-           
-
+            timer = new DispatcherTimer();
+            timer.Tick += new EventHandler(timer_Tick);
+            timer.Interval = new TimeSpan(0, 0, 5);
+            timer.Start();
         }
 
         private void MainWindow_Closing(object sender, CancelEventArgs e)
@@ -40,5 +49,18 @@ namespace _27._11._20_Zoo
             
         }
 
+        private void buttonLoading_Click(object sender, RoutedEventArgs e)
+        {
+            mainGrid.Visibility = Visibility.Visible;
+            mainScroll.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+            mainScroll.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+            buttonLoading.Visibility = Visibility.Hidden;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            buttonLoading.IsEnabled = true;
+            timer.Stop();
+        }
     }
 }
